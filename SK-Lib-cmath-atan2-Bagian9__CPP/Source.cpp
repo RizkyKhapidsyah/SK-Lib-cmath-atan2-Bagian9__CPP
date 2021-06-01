6c111ae1ad45fba8d0b9ae4f2bdc4070d2d1e22b
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-01 07:44:20 +0700

    Add project files.

---
+++ SK-Lib-cmath-atan2-Bagian9__CPP/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*  Source by CPPReference
+    Modified For Learn by Rizky Khapidsyah
+    I.D.E : VS2019
+*/
+
+int main(){
+    // normal usage: the signs of the two arguments determine the quadrant
+    std::cout << "(x:+1,y:+1) cartesian is (r:" << hypot(1, 1)
+        << ",phi:" << atan2(1, 1) << ") polar\n"  // atan2(1,1) = +pi/4, Quad I
+        << "(x:-1,y:+1) cartesian is (r:" << hypot(1, -1)
+        << ",phi:" << atan2(1, -1) << ") polar\n" // atan2(1, -1) = +3pi/4, Quad II
+        << "(x:-1,y:-1) cartesian is (r:" << hypot(-1, -1)
+        << ",phi:" << atan2(-1, -1) << ") polar\n" // atan2(-1,-1) = -3pi/4, Quad III
+        << "(x:+1,y:-1) cartesian is (r:" << hypot(-1, 1)
+        << ",phi:" << atan2(-1, 1) << ") polar\n"; // atan2(-1, 1) = -pi/4, Quad IV
+// special values
+    std::cout << "atan2(0, 0) = " << atan2(0, 0)
+        << " atan2(0,-0) = " << atan2(0, -0.0) << '\n'
+        << "atan2(7, 0) = " << atan2(7, 0)
+        << " atan2(7,-0) = " << atan2(7, -0.0) << '\n';
+
+    _getch();
+    return 0;
+}
